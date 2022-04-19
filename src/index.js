92398d7d575e1350ae2c3275e93b4a1593ceb628
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-04-19 09:26:25 -0600

    feat: add the website theme

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
+import CssBaseline from '@mui/material/CssBaseline'
+import { ThemeProvider } from '@mui/material/styles'
+import { theme } from '../src/styles/theme'
+
 import App from './App'
 import Advertising from './pages/Advertising'
 import Home from './pages/Home'
@@ -15,19 +19,22 @@ import UpdatePages from './pages/UpdatePages'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<Home />} />
-          <Route path="update-pages" element={<UpdatePages />} />
-          <Route path="reservation-list" element={<ReservationList />} />
-          <Route path="manage-rooms" element={<ManageRooms />} />
-          <Route path="hotel-status" element={<HotelStatus />} />
-          <Route path="room-availability" element={<RoomAvailability />} />
-          <Route path="advertising" element={<Advertising />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route index element={<Home />} />
+            <Route path="update-pages" element={<UpdatePages />} />
+            <Route path="reservation-list" element={<ReservationList />} />
+            <Route path="manage-rooms" element={<ManageRooms />} />
+            <Route path="hotel-status" element={<HotelStatus />} />
+            <Route path="room-availability" element={<RoomAvailability />} />
+            <Route path="advertising" element={<Advertising />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 )
